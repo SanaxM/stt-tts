f283adf9e7925cacb4a2f001a063b36669cdfe75
aisha <105862359+saishameh@users.noreply.github.com>
2024-03-26 12:31:10 -0400

    Add files via upload

--- STT.py
+++ STT.py
@@ -8,7 +8,10 @@ model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base")
 model.config.forced_decoder_ids = None
 
 # Read file
-audio_file_path = "/workspaces/codespaces-blank/speech.wav" 
+# audio_file_path = "/workspaces/codespaces-blank/speech.wav" 
+# audio_file_path = "/workspaces/codespaces-blank/money-in-savings.wav"
+# audio_file_path = "/workspaces/codespaces-blank/credit-score.wav" 
+audio_file_path = "/workspaces/codespaces-blank/accessing-account.wav" 
 audio_array, sampling_rate = sf.read(audio_file_path)
 
 # Process audio file
